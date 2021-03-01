f5602bca094bc3875cde59ce9577a538f08f9f27
akssahu3399 <78143104+akssahu3399@users.noreply.github.com>
2021-03-01 14:18:22 +0530

    Add files via upload

---
+++ C-20 BOILER PLATE/sketch.js
@@ -0,0 +1,26 @@
+var a,b;
+
+function setup() {
+  createCanvas(1200,800);
+  a=createSprite(600, 400, 50, 50);
+  b=createSprite(400, 200, 50, 50);
+  a.shapeColor="blue";
+  b.shapeColor="blue";
+}
+ 
+function draw() {
+  background(255,255,255); 
+  b.x=mouseX;
+  b.y=mouseY;
+  if(a.x-b.x<(a.width/2 + b.width/2) && b.x-a.x<(a.width/2 + b.width/2) && a.y-b.y<(a.height/2 + b.height/2) && b.y-a.y<(a.height/2 + b.height/2)) {
+    a.shapeColor="red";
+    b.shapeColor="red";
+  }
+   else {
+    a.shapeColor="blue";
+    b.shapeColor="blue";
+   }
+   console.log(a.x-b.x,a.y-b.y);
+   
+  drawSprites();
+}
